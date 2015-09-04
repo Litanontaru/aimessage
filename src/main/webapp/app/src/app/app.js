a280e39854a07630ae168ec90c426539de5b88b5
Litanontaru <litanontaru@yahoo.com>
2015-09-04 10:08:02 +0400

    add chat as about clone

--- src/main/webapp/app/src/app/app.js
+++ src/main/webapp/app/src/app/app.js
@@ -3,6 +3,7 @@ angular.module( 'ngBoilerplate', [
   'templates-common',
   'ngBoilerplate.home',
   'ngBoilerplate.account',
+  'ngBoilerplate.chat',
   'ui.router'
 ])
 
---
+++ src/main/webapp/app/src/app/chat/chat.js
@@ -0,0 +1,27 @@
+angular.module( 'ngBoilerplate.chat', [
+  'ui.router',
+  'placeholders',
+  'ui.bootstrap'
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'chat', {
+    url: '/chat',
+    views: {
+      "main": {
+        controller: 'AboutCtrl',
+        templateUrl: 'chat/chat.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'What is It?' }
+  });
+})
+
+.controller( 'AboutCtrl', function AboutCtrl( $scope ) {
+  // This is simple a demo for UI Boostrap.
+  $scope.dropdownDemoItems = [
+    "The first choice!",
+    "And another choice for you.",
+    "but wait! A third!"
+  ];
+});
